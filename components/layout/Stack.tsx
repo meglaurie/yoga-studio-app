79e6577af474f33bcc1ad71888f55eb342b52327
meglaurie <la.meg@hotmail.com>
2026-08-07 12:12:59 -0600

    Refactor components to use organized CSS architecture

--- components/layout/Stack.tsx
+++ components/layout/Stack.tsx
@@ -1,36 +1,3 @@
-// import clsx from "clsx";
-
-// interface StackProps {
-//   children: React.ReactNode;
-//   gap?: "sm" | "md" | "lg" | "xl";
-//   className?: string;
-// }
-
-// const gapClasses = {
-//   sm: "gap-2",
-//   md: "gap-4",
-//   lg: "gap-6",
-//   xl: "gap-8",
-// };
-
-// export default function Stack({
-//   children,
-//   gap = "md",
-//   className,
-// }: StackProps) {
-//   return (
-//     <div
-//       className={clsx(
-//         "flex flex-col",
-//         gapClasses[gap],
-//         className
-//       )}
-//     >
-//       {children}
-//     </div>
-//   );
-// }
-
 interface StackProps {
   children: React.ReactNode;
 
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -29,26 +29,22 @@ const navigation = [
 
 export default function Navbar() {
   return (
-    <nav className="background: --color-border rounded border-gray-200 px-2 py-2.5 sm:px-4 dark:bg-gray-900">
-      <ul className="mx-auto flex flex-wrap items-center justify-between">
-        <li>
-          <Link href="/">
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Yoga Studio
-            </span>
-          </Link>
-        </li>
-        {navigation.map((item) => (
-          <li key={item.href}>
-            <Link
-              href={item.href}
-              className="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white"
-            >
-              {item.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <header className="navbar">
+      <nav className="navbar__container">
+        <Link href="/" className="navbar__logo">
+          Stillwater Yoga
+        </Link>
+
+        <ul className="navbar__menu">
+          {navigation.map((item) => (
+            <li key={item.href} className="navbar__item">
+              <Link href={item.href} className="navbar__link">
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
