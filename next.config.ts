d9c7fcaee5aaf29c345e58328328b279bdfee757
meglaurie <la.meg@hotmail.com>
2026-08-25 10:57:19 -0600

    Deleting git pages integrations movingon to vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // Tells Next.js to output static HTML files
-  images: {
-  unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
-  },
-  // If your site URL is username.github.io/repo-name, uncomment the lines below:
-  basePath: '/yoga-studio-app',
-  assetPrefix: '/yoga-studio-app',
+//   output: 'export', // Tells Next.js to output static HTML files
+//   images: {
+//   unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+//   },
+//   // If your site URL is username.github.io/repo-name, uncomment the lines below:
+//   basePath: '/yoga-studio-app',
+//   assetPrefix: '/yoga-studio-app',
 
 };
 
